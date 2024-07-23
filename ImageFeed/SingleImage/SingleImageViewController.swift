b801b448dac0ae467cd8d8ccaf70a8d55a966f22
ssimashov <simashov@gmail.com>
2024-07-23 20:26:56 +0300

    Added single image view controller

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Sergey Simashov on 23.07.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+}
