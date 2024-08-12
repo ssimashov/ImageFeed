a72e4a0acae3484fe0c754ac167a8111b7ea7db6
ssimashov <simashov@gmail.com>
2024-08-12 20:37:05 +0300

    Fixed security

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum Constants {
-    static let accessKey = "W0g9M4o1k7QiIXIyg1VMozjr3mMWXe1_Q3-LbiYiHa4"
+//    static let accessKey = "W0g9M4o1k7QiIXIyg1VMozjr3mMWXe1_Q3-LbiYiHa4"
     static let secretKey = "XKx3MaUMzHWQqnDNSVwag1JomrdzDWwldvQpIx7y7x8"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
