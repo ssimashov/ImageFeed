7cb57a04127abb10aa9ef591f2f9cc8920cd88ff
ssimashov <simashov@gmail.com>
2024-08-13 20:56:18 +0300

    Added auth view controller

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Sergey Simashov on 13.08.2024.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+  
+
+}
